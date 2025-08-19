2239ad981a0f7299c2eb1e1d9f5621c5e5c6ec93
RikaSmosSnea <hiemrika@gmail.com>
2025-08-19 18:04:02 +0700

    register_service

--- aub_tue_wed/routes/api.php
+++ aub_tue_wed/routes/api.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\StudentApiController;
 
-Route::apiResource('students', StudentApiController::class);
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+use App\Http\Controllers\Api\StudentApiController;
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+// Protect the students routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('students', StudentApiController::class);
+    //Other routes
+});
+
